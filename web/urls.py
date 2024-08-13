0fd1114b280ec3a080f16a6cf7b810239dd45b91
ksm2024 <katherinesm@hotmail.cl>
2024-08-12 22:07:05 -0400

    dia12

--- web/urls.py
+++ web/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('text/', views.hola),
     path('', views.index),
-    path('about/', views.acerca),
-    path('welcome/', views.welcome),
+    path('about', views.acerca),
+    path('welcome', views.welcome),
     # path('json/', views.hola_json),
-    path('contacto/', views.contacto, name="contacto"),
-    path('exito/', views.exito),
+    path('contacto', views.contacto, name="contacto"),
+    path('exito', views.exito),
     path('signin/', views.iniciar_sesion),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
